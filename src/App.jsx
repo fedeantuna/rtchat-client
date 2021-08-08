d1dff92df36fa748a2766cc25a13c1bbac0958c4
Federico Antuña <federicoantuna@hotmail.com>
2021-08-08 19:48:54 +0000

    Set up React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Chat from './components/Chat';
 import LandingPage from './components/LandingPage';
 
-const App = () => <LandingPage />;
+const App = () => (
+	<Switch>
+		<Route path='/chat' component={Chat} />
+		<Route path='/' component={LandingPage} />
+	</Switch>
+);
 
 export default App;
