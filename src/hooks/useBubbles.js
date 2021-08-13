0bffd082dfc357671b609be90aaa5912a61fd15a
Federico Antuña <federicoantuna@hotmail.com>
2021-08-13 16:28:18 +0000

    Added useBubbles and useSearch hooks

---
+++ src/hooks/useBubbles.js
@@ -0,0 +1,34 @@
+import { useEffect, useState } from 'react';
+import { getRandomInt } from '../utils/numberUtils';
+
+const useBubbles = () => {
+	const [bubbles, setBubbles] = useState([]);
+
+	useEffect(() => {
+		const styles = [];
+
+		const bubbleCount = process.env.REACT_APP_BUBBLE_COUNT;
+		for (let i = 0; i < bubbleCount; i++) {
+			const diameter = getRandomInt(15, 90);
+			const left = getRandomInt(10, 80);
+			const animationDuration = getRandomInt(5, 12);
+			const animationDelay = getRandomInt(0, 4);
+
+			const bubbleStyle = {
+				width: `${diameter}px`,
+				height: `${diameter}px`,
+				left: `${left}%`,
+				animationDuration: `${animationDuration}s`,
+				animationDelay: `${animationDelay}s`,
+			};
+
+			styles.push(bubbleStyle);
+		}
+
+		setBubbles(styles);
+	}, []);
+
+	return bubbles;
+};
+
+export default useBubbles;
