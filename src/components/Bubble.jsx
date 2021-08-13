410010ef6d6a3c7263dc04f6ce7b38f2fa6eac87
Federico Antuña <federicoantuna@hotmail.com>
2021-08-13 16:29:34 +0000

    Moved Landing Page and Bubbles

--- src/components/Bubble.jsx
+++ src/components/Bubble.jsx
@@ -3,13 +3,19 @@ import PropTypes from 'prop-types';
 
 const Bubble = ({ style }) => (
 	<div
-		className='absolute -bottom-28 w-10 h-10 bg-gray-100 rounded-full opacity-50 animate-rise'
+		className='absolute -bottom-28 bg-gray-100 rounded-full opacity-50 animate-rise'
 		style={style}
 	/>
 );
 
 Bubble.propTypes = {
-	style: PropTypes.shape.isRequired,
+	style: PropTypes.shape({
+		width: PropTypes.string,
+		height: PropTypes.string,
+		left: PropTypes.string,
+		animationDuration: PropTypes.string,
+		animationDelay: PropTypes.string,
+	}).isRequired,
 };
 
 export default Bubble;
