a0bd02d547e5c3a92bb28418f5fc27077ea799c0
Federico Antuña <federicoantuna@hotmail.com>
2021-09-01 12:14:27 +0000

    Modified conversation prop-type and added message

--- src/propTypeModels/conversationPropType.js
+++ src/propTypeModels/conversationPropType.js
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 
 const conversationPropType = PropTypes.shape({
-	id: PropTypes.string.isRequired,
+	userId: PropTypes.string.isRequired,
 	email: PropTypes.string.isRequired,
 	picture: PropTypes.string.isRequired,
 	messages: PropTypes.arrayOf(
